49063b434875b03af3c43bc0a6c395ce9c1b8ca5
Ada Norman <88652665+Adajen@users.noreply.github.com>
2023-01-12 22:05:55 +0600

    Add files via upload

---
+++ part01-Part01_03.OnceUponATime/src/main/java/OnceUponATime.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class OnceUponATime {
+
+    public static void main(String[] args) {
+        // Write your program here
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Write a message:");
+        String message = scanner.nextLine();
+        
+        System.out.println(Message);
+      
+
---
+++ part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -0,0 +1,15 @@
+
+        
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        
+        System.out.println("Hi " + "Ada Lovelace!");
+        
+        
+        
+
+
+    }
+}
